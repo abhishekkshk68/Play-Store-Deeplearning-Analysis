256cdaacd5ac15db7d60bbbadd50a3192e36ac34
Swathi0104 <51449138+Swathi0104@users.noreply.github.com>
2019-06-07 02:53:41 +0100

    Update CombineAllDetailCollections.py
    Added parameter names in the beginning.

--- CombineAllDetailCollections.py
+++ CombineAllDetailCollections.py
@@ -1,10 +1,12 @@
 import os
 import glob
 import pandas as pd
-os.chdir("./Scripts_details_response/FinalDataSet_AndroidApps/Combined_data/Trial")
+path="./FolderNameContainingAppDetailsCSVFiles"
+New_file_name="Combined_CSV_TopFree_Trending.csv"
+os.chdir(path)
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
 #combine all files in the list
 combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-combined_csv.to_csv( "Combined_CSV_TopFree_Trending.csv", index=False, encoding='utf-8-sig')
+combined_csv.to_csv(New_file_name, index=False, encoding='utf-8-sig')
