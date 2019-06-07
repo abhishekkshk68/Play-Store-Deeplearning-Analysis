52f70eddcd928bda7610f29bfcc6d11d40170065
Swathi0104 <51449138+Swathi0104@users.noreply.github.com>
2019-06-07 03:32:22 +0100

    Update MultipleAppsReviews.py

--- MultipleAppsReviews.py
+++ MultipleAppsReviews.py
@@ -1,19 +1,20 @@
 import json
 import unicodecsv as csv
-
 import play_scraper
-
 import pandas as pd
+path_appdetails='./FolderContainingAppDetailsCSV/TOP_FREE_WEATHER.csv'
+path_appreviews_json='./FolderToAddAppReviews/REV_TOP_FREE_WEATHER.json'
+path_appreviews_csv='./FolderToAddAppReviews/REV_TOP_FREE_WEATHER.csv'
 
-datafromcsv = pd.read_csv('./Scripts_details_response/TOP_FREE_WEATHER.csv')
+datafromcsv = pd.read_csv(path_appdetails)
 App_Ids=datafromcsv.app_id
 
 for n in range(len(App_Ids)):
 	print(App_Ids[n])
 ListOfReviews = play_scraper.reviews(App_Ids,10)
-with open(".\\Scripts_details_response\\REV_TOP_FREE_WEATHER.json", "w") as f2:
+with open(".\\FolderToAddAppReviews\\REV_TOP_FREE_WEATHER.json", "w") as f2:
   json.dump(ListOfReviews, f2)
 f2.close()
 
-df = pd.read_json('./Scripts_details_response/REV_TOP_FREE_WEATHER.json')
-df.to_csv('./Scripts_details_response/REV_TOP_FREE_WEATHER.csv', encoding = 'utf-8')
+df = pd.read_json(path_appreviews_json)
+df.to_csv(path_appreviews_csv, encoding = 'utf-8')
